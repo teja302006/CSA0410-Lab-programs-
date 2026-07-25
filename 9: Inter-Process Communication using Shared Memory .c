41a28952b520e7a5a0edfa83a5cd37f504c3ef40
teja M <123854192+teja302006@users.noreply.github.com>
2026-07-25 14:19:50 +0530

    Update and rename 9: Inter-Process Communication using Shared Memory .C to 9: Inter-Process Communication using Shared Memory .c

--- 9: Inter-Process Communication using Shared Memory .c
+++ 9: Inter-Process Communication using Shared Memory .c
@@ -8,22 +8,16 @@ int main()
     key_t key = 1234;
     int shmid;
     char *str;
-
-    // Create shared memory
     shmid = shmget(key, 1024, 0666 | IPC_CREAT);
 
-    // Attach shared memory
     str = (char *)shmat(shmid, NULL, 0);
 
-    // Write data
     strcpy(str, "Hello from Shared Memory!");
 
     printf("Data Written: %s\n", str);
 
-    // Detach shared memory
     shmdt(str);
 
-    // Delete shared memory
     shmctl(shmid, IPC_RMID, NULL);
 
     return 0;
